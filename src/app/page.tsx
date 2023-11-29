a588bb1112574bcfe232704244895b3d114eb874
laflamme-001 <152241223+laflamme-001@users.noreply.github.com>
2023-11-29 07:52:12 -0800

    rough tokenAllocation created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,50 @@
 "use client";
 import { getTokenMetrics, getRequiredTokens } from "./utils";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 export default function Home() {
   useEffect(() => {
     const getAllTokenInfo = async () => {
       const tokenMetrics = await getTokenMetrics(5, 1, 1);
-      const requiredTokens = await getRequiredTokens(5);
+      const requiredTokenMetrics = await getRequiredTokens(5);
     };
 
     getAllTokenInfo();
   }, []);
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Test</h1>
-    </main>
-  );
+  const width = 400;
+  const half = width / 2;
+
+  type tokenAllocationType = {
+    symbol: string;
+    amount: number;
+    color: string;
+    totalSold: number;
+  };
+  const tokenAllocation: tokenAllocationType[] = [
+    {
+      symbol: "Presale",
+      amount: 172.5,
+      color: "#e38627",
+      totalSold: 420,
+    },
+    {
+      symbol: "Listing",
+      amount: 138,
+      color: "#e38627",
+      totalSold: 420,
+    },
+    {
+      symbol: "Fees",
+      amount: 3.45,
+      color: "#e38627",
+      totalSold: 420,
+    },
+    {
+      symbol: "Unlocked",
+      amount: 106.05,
+      color: "#e38627",
+      totalSold: 420,
+    },
+  ];
+
+  return <svg className="mt-12" width={width} height={width}></svg>;
 }
