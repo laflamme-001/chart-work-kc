0d82eb207aad4e3c715958f775d9bca199d217d8
laflamme-001 <152241223+laflamme-001@users.noreply.github.com>
2023-11-30 10:17:37 -0800

    changed config data

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    seed: "L5kXfz1aF4vbTB5huTZ7jPi17L6SqRtWrHr3dDVhSS5em9wgWKDL",
-    koinCityContract: "1FZnS98QunDkzVjMz4G8BRSTAZyYCaPHhm",
+    seed: process.env.SIGNER_KEY,
+    koinCityContract: process.env.CONTRACT_ADDRESS,
   },
 };
 
